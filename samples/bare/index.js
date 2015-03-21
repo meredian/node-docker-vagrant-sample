b08065e9ef567cb29a7ce7ea7c14e9186a22ce02
Anton Sidelnikov <ndmeredian@gmail.com>
2015-03-22 00:14:40 +0600

    Moved test handler to server root

--- samples/bare/index.js
+++ samples/bare/index.js
@@ -4,11 +4,11 @@ var app = express();
 app.counter = 0;
 app.port = 3000;
 
-app.get('/sample', function(req, res, next) {
+app.get('/', function(req, res, next) {
     console.log("Counter request received; return value: " + app.counter);
     res.json({ counter: app.counter++ });
 });
 
 app.listen(app.port, function() {
-    console.log("Frontend is running on port " + app.port);
+    console.log("Server is running on port " + app.port);
 });
