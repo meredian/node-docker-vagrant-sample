35ca5d78102f65ef53b4d19c309326aafc2da2b4
Anton Sidelnikov <ndmeredian@gmail.com>
2015-03-24 23:19:46 +0600

    Add 03_fullstack sample

---
+++ samples/03_fullstack/README.md
@@ -0,0 +1,9 @@
+Server dockerized with all dependencies
+=======================================
+
+Server without any dependencies, just some modules to install. Chech `localhost:3000` to see if it is working
+
+To run server after installing vagrant and docker:
+
+ - `vagrant up` to launch environment
+ - `vagrant docker-logs` to see agregated logs from docker containers
---
+++ samples/03_fullstack/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var redis = require("redis");
+
+var app = express();
+
+app.counter = 0;
+app.port = 3000;
+
+// Hostname 'redis' will be added to container's /etc/hosts and will be recognized by server
+var redisHost = (process.env.NODE_ENV === 'docker') ? "redis" : "127.0.0.1";
+
+app.redis = redis.createClient(6379, redisHost);
+
+app.redis.on("error", function (err) {
+    console.log("Redis error: " + err);
+});
+
+app.get('/', function(req, res, next) {
+    console.log("GET some_redis_key ");
+    app.redis.get("some_redis_key", function(err, value) {
+        if (err) {
+            res.json({error: err});
+        } else {
+            if (value) {
+                res.json({redis_key_value: value});
+            } else {
+                res.json({error: "No Redis value set"});
+            }
+        }
+        next();
+    });
+});
+
+app.get('/set/:value', function(req, res, next) {
+    var newValue = req.params.value;
+    if (!newValue) {
+        res.json({error: "New value missing, must be /set/new_value"});
+        return next();
+    }
+    console.log("SET some_redis_key " + newValue);
+    app.redis.set("some_redis_key", newValue, function(err, value) {
+        if (err) {
+            res.json({error: err});
+        } else {
+            res.json({redis_key_value: newValue});
+        }
+        next();
+    });
+});
+
+app.listen(app.port, function() {
+    console.log("Server is running on port " + app.port);
+});
